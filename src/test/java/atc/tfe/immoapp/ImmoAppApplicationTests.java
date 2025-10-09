316029dff818969518619f22f37e08cb1b4f4f36
cedricLet <clettany@gmail.com>
2025-10-09 22:06:04 +0200

    add frontend Angular skeleton

--- src/test/java/atc/tfe/immoapp/ImmoAppApplicationTests.java
+++ src/test/java/atc/tfe/immoapp/ImmoAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ImmoAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
