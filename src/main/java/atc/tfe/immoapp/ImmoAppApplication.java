a14a242e528d8151f454c76c1f763247f3e71c10
cedricLet <clettany@gmail.com>
2025-10-09 21:54:26 +0200

    initial Spring + Angular workspace

---
+++ src/main/java/atc/tfe/immoapp/ImmoAppApplication.java
@@ -0,0 +1,13 @@
+package atc.tfe.immoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoAppApplication.class, args);
+	}
+
+}
